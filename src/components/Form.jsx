938b021795845811cc301d5f32ae877d6c547ed0
Shan Siddiqui <49912126+shansiddiqui94@users.noreply.github.com>
2024-03-04 15:19:44 -0500

    created Form component, created db.json

---
+++ src/components/Form.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import {useState} from "react"
+
+function Form() {
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [post, setPost] = useState("")
+
+    function handleName(e){
+        setName(e.target.value)
+    }
+
+    function handleEmail(e){
+        setEmail(e.target.value)
+    }
+
+
+    function handlePost(e){
+        setPost(e.target.value)
+    }
+
+    function handleSubmit(e){
+        e.preventDefault()
+        alert("Successfully Posted")
+    }
+
+
+  return (
+
+    <div>
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="name" onChange={handleName}/>
+            <input type="text" placeholder="email" onChange={handleEmail}/>
+            <textarea rows={4} cols={50} placeholder="Add your blog..." onChange={handlePost}/>
+
+        </form>
+        <button type='submit'>Submit</button>
+    </div>
+  )
+}
+
+export default Form
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>
-        <h1>This is LULe</h1>
+    <div  className="header">
+        <h1>This is LUL˜e</h1>
     </div>
   )
 }
